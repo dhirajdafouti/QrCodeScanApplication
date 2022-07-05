9b084cf0980ca73862adcc0e1a27693a08febf67
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-07-05 21:47:08 +0530

    Initial comment for scanner application

--- app/src/main/java/com/project/basicqrcodescan/MainActivity.kt
+++ app/src/main/java/com/project/basicqrcodescan/MainActivity.kt
@@ -1,58 +1,101 @@
 package com.project.basicqrcodescan
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.util.Log
+import android.util.Log.DEBUG
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.project.basicqrcodescan.databinding.ActivityMainBinding
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 class MainActivity : AppCompatActivity() {
+    //https://www.youtube.com/watch?v=drH63NpSWyk
+    //https://www.youtube.com/watch?v=Sb4avOp7D_k
+    // https://www.youtube.com/watch?v=iryHXuwuJ3Q
+    //https://www.youtube.com/watch?v=6OFniVVzmgQ
+    // https://github.com/niharika2810/zxing-android-embedded
+    //https://www.youtube.com/watch?v=asl1mFtkMkc
+    //https://github.com/prof18/Secure-QR-Reader
+    //https://youtu.be/Sb4avOp7D_k
+    //https://youtu.be/drH63NpSWyk
+    //https://www.youtube.com/watch?v=wfucGSKngq4
+    //https://www.youtube.com/watch?v=AiNi9K94W5c
+    //https://www.youtube.com/playlist?list=PLirRGafa75rQOi3so_ngAHqDmq_Djifwu
+    //https://www.youtube.com/watch?v=6OFniVVzmgQ
+    //https://www.youtube.com/watch?v=wfucGSKngq4
+    //https://github.com/zxing/zxing/
+    private var flashActive = false
+    private var hideFlashActive = false
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+        setUpPermission()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun setUpPermission() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
         }
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE ->
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,
+                        "The Permissions are not Granted For Camera Access!!!",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d(TAG, "Permission Granted!!")
+                }
+        }
+        return super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
---
+++ app/src/main/java/com/project/basicqrcodescan/WelcomeActivity.kt
@@ -0,0 +1,31 @@
+package com.project.basicqrcodescan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.project.basicqrcodescan.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+    private lateinit var text:TextView
+    private lateinit var button:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        text=findViewById(R.id.text)
+        button=findViewById(R.id.button_click)
+        button.setOnClickListener {
+            startScanningActivity()
+        }
+
+    }
+
+    private fun startScanningActivity() {
+      val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+}
